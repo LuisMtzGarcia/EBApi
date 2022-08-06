be6634f3e2eb1cd2064769be4425d05139de688b
LuisMtzGarcia <ing.luismtzgarcia@gmail.com>
2022-08-06 09:10:32 -0500

    Setup URL files.

---
+++ EBApi/urls.py
@@ -0,0 +1,11 @@
+"""Defines URL patterns for EBApi."""
+
+from django.urls import path
+
+from . import views
+
+app_name = 'EBApi'
+urlpatterns = [
+    # Home page
+    path('', views.index, name='index'),
+]
